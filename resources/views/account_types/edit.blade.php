2fbe5490babc0e4385e6348210be258e673f090b
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-04-28 12:33:45 +0300

    edit account types

--- resources/views/account_types/edit.blade.php
+++ resources/views/account_types/edit.blade.php
@@ -10,28 +10,29 @@
                 <form method="POST" action="{{ route('account-types.update') }}">
                     @csrf
     
-                    <div class="form-group">
+                    <div class="form-group mb-1">
                         <label for="year">Account Name</label>
-                        <input type="text" name="name" placeholder="Enter Account Name" id="year" class="form-control" required>
+                        <input type="text" name="name" value="{{$account->name }}" id="year" class="form-control" required>
+                        <input type="hidden" name="id" value="{{ $account->id}}">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mb-1">
                         <label for="year">Account Description</label>
-                        <textarea name="description" class="form-control" placeholder="Enter Description"  rows="3"></textarea>
+                        <textarea name="description" class="form-control" value="{{$account->description }}"  rows="3">{{$account->description }}</textarea>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mb-1">
                         <label for="year">Account Code</label>
-                        <input type="number" min="0" placeholder="Enter Account Code" name="account_code"  class="form-control" required>
+                        <input type="number" min="0" value="{{$account->account_code }}" name="account_code"  class="form-control" required>
                     </div>
     
-                    <div class="form-group">
+                    <div class="form-group mb-1">
                         <label for="start_date">Start Code</label>
-                        <input type="number" min="0" name="min_code" id="min_code" placeholder="Enter Minimum Account Code" class="form-control" required>
+                        <input type="number" min="0" name="min_code" id="min_code" value="{{$account->min_code }}" class="form-control" required>
                     </div>
     
-                    <div class="form-group">
+                    <div class="form-group mb-1">
                         <label for="end_date">End Code</label>
-                        <input type="number" min="0" name="max_code" id="max_code" placeholder="Enter Maximum Account Code" class="form-control" required>
+                        <input type="number" min="0" name="max_code" id="max_code" value="{{$account->max_code }}" class="form-control" required>
                     </div>
                     <hr>
                     <button type="submit" class="btn btn-info float-end">Submit</button>
