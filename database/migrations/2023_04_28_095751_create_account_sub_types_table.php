42d2824294a948f84aa1ed1e6a72392fcf25ebdc
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-04-28 13:02:00 +0300

    added subaccount table

--- database/migrations/2023_04_28_095751_create_account_sub_types_table.php
+++ database/migrations/2023_04_28_095751_create_account_sub_types_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('account_sub_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->foreignId('account_id');
+            $table->boolean('status')->default(0);
+            $table->integer('account_code')->unique();
+            $table->integer('min_code')->unique();
+            $table->integer('max_code')->unique();
+            $table->foreignId('created_by');
             $table->timestamps();
+
+            // Relation Constraints
+            $table->foreign('account_id')->references('id')->on('account_types')->onDelete('cascade');
+
         });
     }
 
